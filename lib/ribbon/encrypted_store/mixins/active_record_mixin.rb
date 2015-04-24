e2652c48e9353799dec76d8cf0fea016e99f2ca7
Kayvon Ghaffari <kaghaffa@gmail.com>
2015-04-24 00:30:20 -0700

    continued working on mixin

--- lib/ribbon/encrypted_store/mixins/active_record_mixin.rb
+++ lib/ribbon/encrypted_store/mixins/active_record_mixin.rb
@@ -9,19 +9,27 @@ module Ribbon::EncryptedStore
       end # Module Methods
 
       module ClassMethods
+        def _encryption_key
+          config.decrypt_key(config.decrypt_key(EncryptionKey(self.encrypted_key_id)))
+        end
+
         def _crypto_hash
-          @_crypto_hash ||= (self.encrypted_store && CryptoHash.decrypt(self.encrypted_store)) || CryptoHash.new
+          @_crypto_hash ||= CryptoHash.decrypt(self.encrypted_store, _encryption_key)
+        end
+
+        def attr_encrypted(*args)
+          args.each { |arg|
+            define_method(arg) { _encrypted_store_get(field) }
+            define_method("#{arg}=") { |value| _encrypted_store_set(field, value) }
+          }
         end
       end # ClassMethods
 
       ##
       # Instance Methods
-
-      def attr_encrypted(*args)
-        args.each { |arg|
-          define_method(arg) { _encrypted_store_get(field) }
-          define_method("#{arg}=") { |value| _encrypted_store_set(field, value) }
-        }
+      ##
+      def _crypto_hash
+        self.class._crypto_hash
       end
 
       def _encrypted_store_get(field)
@@ -34,7 +42,9 @@ module Ribbon::EncryptedStore
       end
 
       def _encrypted_store_save
-        self.encrypted_store = @_crypto_hash.encrypt
+        if @_encrypted_store_changed
+          self.encrypted_store = @_crypto_hash.encrypt
+        end
       end
     end # ActiveRecordMixin
   end # Mixins
