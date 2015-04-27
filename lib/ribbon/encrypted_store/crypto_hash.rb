df3cdc309e71384df2acf25f39dcd42ef710cc12
Kayvon Ghaffari <kaghaffa@gmail.com>
2015-04-27 16:58:33 -0700

    Fixed up CRC / checksum handling for encrypted data

--- lib/ribbon/encrypted_store/crypto_hash.rb
+++ lib/ribbon/encrypted_store/crypto_hash.rb
@@ -29,61 +29,67 @@ module Ribbon::EncryptedStore
       _append_crc32(data_packet)
     end
 
-    def self.decrypt(dek, data)
-      return CryptoHash.new if data.nil?
-      salt, data = _split_binary_data(data)
+    class << self
+      def decrypt(dek, data)
+        return CryptoHash.new if data.nil?
+        salt, data = _split_binary_data(data)
 
-      begin
-        key, iv = _keyiv_gen(dek, salt)
+        begin
+          key, iv = _keyiv_gen(dek, salt)
 
-        decryptor = OpenSSL::Cipher::AES256.new(:CBC).decrypt
-        decryptor.key = key
-        decryptor.iv = iv
+          decryptor = OpenSSL::Cipher::AES256.new(:CBC).decrypt
+          decryptor.key = key
+          decryptor.iv = iv
 
-        new_hash = JSON.parse(decryptor.update(data) + decryptor.final)
-        new_hash = Hash[new_hash.map { |k,v| [k.to_sym, v] }]
-        CryptoHash.new(new_hash)
-      rescue Exception => e
-        raise
-      rescue OpenSSL::Cipher::CipherError
-        raise
+          new_hash = JSON.parse(decryptor.update(data) + decryptor.final)
+          new_hash = Hash[new_hash.map { |k,v| [k.to_sym, v] }]
+          CryptoHash.new(new_hash)
+        rescue Exception => e
+          raise
+        rescue OpenSSL::Cipher::CipherError
+          raise
+        end
       end
-    end
+
+      def _keyiv_gen(key, salt)
+        key_and_iv = OpenSSL::PKCS5.pbkdf2_hmac(
+          key,
+          salt,
+          4096,
+          48,
+          OpenSSL::Digest::SHA256.new
+        )
+
+        key = key_and_iv[0..31]
+        iv  = key_and_iv[32..-1]
+
+        [key, iv]
+      end
+
+      def _split_binary_data(encrypted_data)
+        # Split encrypted data and CRC
+        bytes = encrypted_data.bytes
+
+        version     = bytes[0]
+        salt_length = bytes[1]
+
+        salt_start_index = 2
+        salt_end_index   = salt_start_index + salt_length - 1
+        salt = bytes[salt_start_index..salt_end_index].pack('c*')
+        data = bytes[salt_end_index+1..-5].pack('c*')
+
+        crc = bytes[-4..-1]
+        raise Errors::ChecksumFailedError unless crc == _calc_crc32(encrypted_data[0..-5]).bytes
+
+        [salt, data]
+      end
+
+      def _calc_crc32(data)
+        [Zlib.crc32(data)].pack('N')
+      end
+    end # Class Methods
 
     private
-
-    def self._keyiv_gen(key, salt)
-      key_and_iv = OpenSSL::PKCS5.pbkdf2_hmac(
-        key,
-        salt,
-        4096,
-        48,
-        OpenSSL::Digest::SHA256.new
-      )
-
-      key = key_and_iv[0..31]
-      iv  = key_and_iv[32..-1]
-
-      [key, iv]
-    end
-
-    def self._split_binary_data(encrypted_data)
-      # Split encrypted data and CRC
-      bytes, crc = [encrypted_data[0..-9].bytes, encrypted_data[-8..-1]]
-
-      version     = bytes[0]
-      salt_length = bytes[1]
-
-      salt_start_index = 2
-      salt_end_index   = salt_start_index + salt_length - 1
-      salt = bytes[salt_start_index..salt_end_index].pack('c*')
-      data = bytes[salt_end_index+1..-1].pack('c*')
-
-      crc = encrypted_data[-8..-1]
-      raise Errors::ChecksumFailedError unless crc == Zlib.crc32(encrypted_data[0..-9]).to_s(16)
-
-      [salt, data]
-    end
 
     def _encrypted_data_header(salt)
       "\x01" + salt.bytes.length.chr + salt
@@ -94,7 +100,11 @@ module Ribbon::EncryptedStore
     end
 
     def _append_crc32(data)
-      data + Zlib.crc32(data).to_s(16)
+      data + _calc_crc32(data)
+    end
+
+    def _calc_crc32(data)
+      self.class._calc_crc32(data)
     end
   end # CryptoHash
 end # Ribbon::EncryptedStore
