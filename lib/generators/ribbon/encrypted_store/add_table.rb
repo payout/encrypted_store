70232ccf1daecff60ec09cf3576e388fb9857151
Kayvon Ghaffari <kaghaffa@gmail.com>
2015-04-27 18:01:53 -0700

    Finished add_table generator

--- lib/generators/ribbon/encrypted_store/add_table.rb
+++ lib/generators/ribbon/encrypted_store/add_table.rb
@@ -4,7 +4,8 @@ module Ribbon::EncryptedStore
     argument :table_name, :type => :string
 
     def create_migrations
-      generate "migration", "add_encrypted_store_to_#{table_name.underscore} encryption_key_id:integer encrypted_store:binary"
+      generate "migration", "add_encrypted_store_to_#{table_name} encryption_key_id:integer encrypted_store:binary"
+      rake "db:migrate"
     end
   end # AddTable
 end # Ribbon::EncryptedStore
