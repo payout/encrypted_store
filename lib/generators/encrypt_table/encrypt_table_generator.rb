2e0c211fd651e77869e2e852257b38ab201d62ba
Kayvon Ghaffari <kaghaffa@gmail.com>
2015-04-28 14:27:02 -0700

    finished generators

---
+++ lib/generators/encrypt_table/encrypt_table_generator.rb
@@ -0,0 +1,9 @@
+class EncryptTableGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  argument :table_name, :type => :string
+
+  def create_migrations
+    generate "migration", "add_encrypted_store_to_#{table_name} encryption_key_id:integer encrypted_store:binary"
+    rake "db:migrate"
+  end
+end # EncryptTableGenerator
